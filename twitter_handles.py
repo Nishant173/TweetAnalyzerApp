64d30420dffee189cab26db2297621af39129f98
nishant173 <nishant.rao173@gmail.com>
2020-11-28 23:43:03 +0530

    Updated requirements.txt

--- twitter_handles.py
+++ twitter_handles.py
@@ -1,5 +1,29 @@
 usernames = [
-    'realDonaldTrump', 'BarackObama', 'katyperry', 'justinbieber', 'rihanna', 'taylorswift13', 'Cristiano'
+    # 'Lewag0atski', 'toyintx', 'fervieg', 'ArdoozBayern', 'BRAZZORZ', 'javisjames11',
+    # 'Oncheedo', 'escarlettes_', 'Slimesteiger', 'straightredblog', 'BundeSabeh', 'mike_rl9',
+    # 'lukas_lukassi', 'SterndesSudenss', 'lehm_FCB', 'MarkodoniaFT', 'leighFCB', 'naouresTN',
+    # 'Red_Roscoe', 'DeadlyComan', 'BavarianTweets', 'bayernxashlee', 'NeuerBoateng', 'neongoretzka',
+    # 'bayernismus', 'BalgaFCB', 'packmasss'
+    
+    # 'Lewag0atski', 'toyintx', 'escarlettes_', 'Slimesteiger', 'BundeSabeh', 'lehm_FCB', 'MarkodoniaFT', 'leighFCB',
+    # 'bayernismus', 'BalgaFCB', 'packmasss', 'lukas_lukassi', 'fervieg', 'ArdoozBayern', 'mike_rl9', 'BundeSabeh',
+    # 'Oncheedo', 'Red_Roscoe', 'SuckerForBayern', 'navvidu', 'DemetriusXVII', 'ComanProdigy', 'Fear_Bavaria', 'nishant173',
+    # 'Basti1_basti1'
+
+    # 'NotDwaith', 'viraj_sg', 'thomasjv93', 'kishorebarker', 'nishant173', 'nikhita_raaj'
+    # 'nishant173', 'nikhita_raaj',
+    # 'prakharisfab', 'SreenivasRathin',
+    'xGPhilosophy', 'Caley_graphics',
+
+    # 'EiFSoccer', 'R3greting'
+    
+    # 'TonyFergusonXT'
+
+    # 'realDonaldTrump', 'BarackObama', 'katyperry', 'justinbieber', 'rihanna', 'taylorswift13',
+    # 'Cristiano', 'elonmusk', 'joerogan', 'neiltyson', 'MikeTyson', 'TheNotoriousMMA', 'stylebender'
+    
+    # 'CaulkinTheTimes', 'OptaJoe', 'samuelluckhurst', 'FantasyScout1', 'FootbalIhub',
+    # 'sidlowe', 'honigstein', 'DeludedBrendan'
     ]
 
-number_of_tweets = 150
+number_of_tweets = 6000
